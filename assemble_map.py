e5cc0d37632b4a94deba945c85b71ce2cdff8306
Bernardito-Luis <bernardito.luis.el.gorra@gmail.com>
2014-04-04 17:15:50 +0400

    Reworked cron job. Now it does generate some maps and put the result into DB every 7 minutes. Main app also generates the rest of the map using the treasure map from the DB.

---
+++ assemble_map.py
@@ -0,0 +1,124 @@
+﻿# -*- coding: utf-8 -*-
+
+# Wariors of the night! Assemble!
+from random import shuffle
+
+# 3 symbols is a cell code number after is a quantity of a cell
+
+map_legend = {
+    'emp18': u'Пустое поле. Попрыгайте на полянке, порадуйтесь солнышку =)',
+
+    'mss03': u'Передвижение по стрелке прямо',
+    'mds03': u'Передвижение по стрелке диагонально',
+    'msb03': u'Передвижение по стрелке прямо, выбор из 2х',
+    'mdb03': u'Передвижение по стрелке диагонально, выбор из 2х',
+    'mtd03': u'Передвижение по стрелке, выбор из 3х',
+    'mfd03': u'Передвижение по стрелке диагонально, выбор из 4х',
+    'mfs03': u'Передвижение по стрелке прямо, выбор из 4х',
+    'mlk02': u'Ход конем!',
+    'ice06': u'Лёд',
+    'cav04': u'Пещера',
+    'pln01': u'Самолет!',
+    'cnn02': u'Пушка',
+    'fob02': u'Воздушный шар',
+
+    '2mc05': u'Клетка, по которой надо топать 2 хода, если только не..',
+    '3mc04': u'Клетка, по которой надо топать 3 хода, если только не..',
+    '4mc02': u'Клетка, по которой надо топать 4 хода, если только не..',
+    '5mc01': u'Клетка, по которой надо топать 5 хода, если только не..',
+
+    '5cc': 'Сuундук с пятью золотыми!!!',
+    '4cc': 'Сuундук с четырьмя золотыми!!',
+    '3cc': 'Сuундук с тремя золотыми!',
+    '2cc': 'Сuундук с двумя золотыми',
+    '1cc': 'Сuундук с одним золотым',
+    'sgn': 'Сuокровища испанского галеона! Скорее тащи!!!',
+
+    '1br03': u'1 бутылка рома',
+    '2br02': u'2 бутылочки рома',
+    '3br01': u'3 прекрасных бутылочки рома',
+
+    'trp03': u'Ловушка!',
+    'crd04': u'Крокодил!',
+    'gnb01': u'Людоед!!',
+    'frt02': u'Форт =)',
+    'ftu01': u'Форт с аборигеночкой ;)',
+    'crb01': u'Каррамба!',
+    'phs01': u'Маяк',
+    'bgn01': u'К Вам присоединятеся Бен Ган',
+    'pst01': u'К Вам присоединятеся священник',
+    'frd01': u'К Вам присоединятеся Пятница, это ведь лучше чем понедельник?)',
+    'esr04': u'Ром, тот, что нельзя забрать с собой',
+    'erq01': u'Землетрясение!',
+    'jng03': u'Джунгли -_-',
+    'drg02': u'Конопля %-)',
+
+}
+
+# cur_map defines where the treasures are. meanwhile it is a string like '1cc 7 1cc 19 1cc 48 1cc 71 ...'
+def assemble(cur_map):
+    # split the treasures-string make list
+    cur_map_list = cur_map.split(' ')
+    
+    # other cells to be added
+    cells_to_add = []
+    for k in map_legend.keys():
+        # exclude treasures
+        if (k == 'sgn') or (k[1]+k[2] == 'cc'):
+            continue
+        for i in range(int(k[-2]+k[-1])):
+            cells_to_add.append(k)
+
+    shuffle(cells_to_add)
+
+
+    # create empty island
+    island = []
+    for i in range(121):
+        island.append('_')
+
+    # block corners
+    island[0] = 'b'
+    island[10] = 'b'
+    island[11*10] = 'b'
+    island[11*10+10] = 'b'
+
+    # place gold
+    buf = '~_^'
+    even = True
+    for i in cur_map_list:
+        if even:
+            buf = i
+            even = False
+        else:
+            # check if the cell is really empty
+            if island[int(i)] == '_':
+                island[int(i)] = buf
+            else:
+                print 'An error occurred. Attempt to fill not an empty field'
+                break
+            even = True
+
+
+
+    # place other
+    # bad decision?
+    for i in range(121):
+        if island[i] == '_':
+            island[i] = cells_to_add.pop()
+            
+    # generate json-suit list
+    json_list = []    
+    for cell in island:
+        if cell == "b":
+            json_list.append('small_pics/wat.png')
+            continue
+        if cell[:3] == "emp":
+            json_list.append('small_pics/em1.png')
+            continue
+            
+        json_list.append('small_pics/'+cell[:3]+'.png')
+
+    
+    return json_list
+    
---
+++ map_generator.py
@@ -0,0 +1,204 @@
+# -*- coding: utf-8 -*-
+
+# ..................................................................................................................120
+import webapp2
+from google.appengine.ext import ndb
+
+import random
+# procedures, functions and so on to geberate maps
+# =====================================================================================================================
+# we are going to add them
+possible_maps = []
+
+world_treasures = ['3gn', '5cc', '4cc', '4cc', '3cc', '3cc', '3cc', '2cc',
+                   '2cc', '2cc', '2cc', '2cc', '1cc', '1cc', '1cc', '1cc',
+                   '1cc']
+
+# fill random with treasures
+def add_treasures(island):
+    candidate = 0
+    treasures = ['3gn', '5cc', '4cc', '4cc', '3cc', '3cc', '3cc', '2cc', '2cc',
+                 '2cc', '2cc', '2cc', '1cc', '1cc', '1cc', '1cc', '1cc']
+
+    while treasures:
+        while island[candidate] != '_':
+            candidate = random.randrange(121)
+
+        # place a treasure
+        island[candidate] = treasures.pop()
+
+# map_count is used to count successful maps or smth on =\
+def calculate_distances(island_o, possible_maps):
+    #dbg
+    dists = []
+    island = island_o[:]
+    add_treasures(island)
+    handicap = [0,0,0,0]
+    treasures_map = []
+    for place in island:
+        if place in world_treasures:
+            # found a chest
+            one_chest = (place, island.index(place))
+            treasures_map.append(one_chest)
+            #print "this", island.index(place), place
+            y = (island.index(place))/11
+            x = island.index(place) - y*11
+            # mark a cell "counted"
+            island[island.index(place)] = 'c'+place
+            dists.append(max(abs(0-y),abs(5-x))+1)
+            dist1 = float(place[0])/(max(abs(0-y),abs(5-x))+1)
+            dist2 = float(place[0])/(max(abs(5-y),abs(10-x))+1)
+            dist3 = float(place[0])/(max(abs(10-y),abs(5-x))+1)
+            dist4 = float(place[0])/(max(abs(5-y),abs(0-x))+1)
+            handicap[0] += dist1
+            handicap[1] += dist2
+            handicap[2] += dist3
+            handicap[3] += dist4
+
+
+    #print "!!:", handicap
+    value = max(handicap) - min(handicap)
+    buffer_string = ""
+    if (value) < 0.07:
+        #print 'h', handicap
+        #print 'd', dists
+        
+        
+        treasures_map = sorted( treasures_map, key=lambda x: x[0])
+        buf = tuple(treasures_map)
+        # all_maps-variable is deprecated
+        
+        for elem in treasures_map:
+            buffer_string += str(elem[0])+" "+str(elem[1])+" "
+            
+        #print "Success!!", value
+    if buffer_string != "":
+        print "Success!!"
+        possible_maps.append(buffer_string)
+        
+    
+# prepare template-map        
+def prepare_pseudo():
+    pseudo = []
+
+    # empty pseudo
+    for i in range(121):
+        pseudo.append('_')
+
+    # block corners
+    pseudo[0] = 'b'
+    pseudo[10] = 'b'
+    pseudo[11*10] = 'b'
+    pseudo[11*10+10] = 'b'
+    
+    return pseudo
+# =====================================================================================================================
+
+
+def map_key(map_name='Map'):
+    """Constructs a Datastore key for a map entity with map_name."""
+    return ndb.Key('Map', map_name)
+
+
+class TreasureMap(ndb.Model):
+    map_id = ndb.IntegerProperty()
+    info = ndb.StringProperty()
+    
+    # can become deprecated
+    def add_map(self):
+        map_query = TreasureMap.query(TreasureMap.map_id == 0)
+        map_got = map_query.fetch(1)
+        if int(map_got[0].info)/0xFFFF == 0:
+            self.put()
+
+    
+class MapGenerator(webapp2.RequestHandler):
+    def get(self):
+    
+        map_query = TreasureMap.query(TreasureMap.map_id == 0)
+        map_got = map_query.fetch(1)
+        zero_record_exists = False
+        try:
+            # increase number of maps (deprecated)
+            # map_got[0].info = str( int(map_got[0].info) + 1)
+            # map_got[0].put()
+            
+            # very important print, it generates the exception
+            print map_got[0].info
+            # set the OK-flag
+            zero_record_exists = True
+            
+            
+        except (IndexError):
+            # initialize database with a head record
+            print "DB initialized"
+            map = TreasureMap(parent=map_key())
+            map.map_id = 0
+            # head record info field has the following legend:
+            #       [2bytes - pointer to the current map][2bytes - maps quantity]
+            # besides it is a string %-)
+            map.info = str(0x10000)
+            map.put()
+            
+        if zero_record_exists:
+            # maps that already exists
+            cur_quantity = int(map_got[0].info) % 0x10000
+            self.response.write('>>>>>>>>>>>>>>>>>>>> cur:' + str(cur_quantity))
+            # create new map (deprecated)
+            # map = TreasureMap(parent=map_key())
+            # map.map_id = int(map_got[0].info)
+            
+            pseudo = prepare_pseudo()
+
+            new_map = 'place new map-string here'
+            possible_maps = []
+            for i in range(25000):
+                calculate_distances(pseudo, possible_maps)            
+
+            # add maps if possible
+            if len(possible_maps) != 0:
+                for map_string in possible_maps:
+                    # increase == add one map
+                    cur_quantity += 1
+                    map = TreasureMap(parent=map_key())
+                    map.map_id = cur_quantity
+                    map.info = map_string[:-1]
+                    map.put()
+                    if cur_quantity == 0xFFFF:
+                        return
+            
+            #print "!!!!!!!!!!!!!!!!!!!!!!!!! map_got[0].info:", map_got[0].info
+            map_got[0].info = str((int(map_got[0].info) / 0x10000)*0x10000 + cur_quantity)
+            map_got[0].put()
+                    
+                
+            # try:
+                # map.info = possible_maps[0]
+            # except (IndexError):
+                # map.info = "Calculation failed =("
+            #map_got[0].put()
+            # add new map    
+            
+            #map.add_map()
+            
+        # # set head record only once
+        # init_db()
+        # # create new map
+        # map = TreasureMap(parent=map_key())
+        # # see the head record
+        # map_query = TreasureMap.query(TreasureMap.map_id == 0)
+        # map_got = map_query.fetch(1)
+        # if map_got[0].info == 0:
+            # map.map_id = map_got[0].info + 1
+            # map.info = 0xACE
+            # map_got[0].put()
+        # map.add_map()
+        
+        
+application2 = webapp2.WSGIApplication([
+        ('/cron_tasks/map_generator', MapGenerator),
+], debug=True)
+
+# if __name__ == '__main__':
+    # run_wsgi_app(application2)
+        
